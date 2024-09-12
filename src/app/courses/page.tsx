1a245bdb0040dc4727f4a36376f6504a214c3752
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-09-12 12:18:51 +0530

    Updated Courses Page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -29,9 +29,9 @@ const Page = () => {
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src={course.thumbnail || "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-                  height="1000"
-                  width="1000"
+                  src={course.image || "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                  height={1000}
+                  width={1000}
                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                   alt={course.title || "Course Thumbnail"}
                 />
@@ -40,7 +40,7 @@ const Page = () => {
                 <CardItem
                   translateZ={20}
                   as={Link}
-                  href={course.link || "https://twitter.com/mannupaaji"}
+                  href="#"
                   target="_blank"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
